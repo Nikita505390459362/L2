c75cc64c781cc24c0a1af1370604f7309cf55ea9
Nikita <n-i-k-i-t-a.2004@mail.ru>
2024-12-04 13:00:17 +0300

    1

---
+++ AeroFlot.h
@@ -0,0 +1,33 @@
+#ifndef AEROFLOT_H
+#define AEROFLOT_H
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+class AEROFLOT {
+private:
+    std::string destination;
+    std::string flightNumber;
+    std::string aircraftType;
+
+public:
+    AEROFLOT();
+    AEROFLOT(const std::string &dest, const std::string &num, const std::string &type);
+    AEROFLOT(const AEROFLOT &other);
+
+    virtual ~AEROFLOT();
+
+    std::string getDestination() const;
+    void setDestination(const std::string &dest);
+    std::string getFlightNumber() const;
+    void setFlightNumber(const std::string &num);
+    std::string getAircraftType() const;
+    void setAircraftType(const std::string &type);
+
+    AEROFLOT& operator=(const AEROFLOT &other);
+    friend std::ostream& operator<<(std::ostream &os, const AEROFLOT &flight);
+    friend std::istream& operator>>(std::istream &is, AEROFLOT &flight);
+};
+
+#endif // AEROFLOT_H
